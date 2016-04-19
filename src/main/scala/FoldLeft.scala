16e0eee03ed0c496f9a4eebee67de812fcdcf29b
jonkey <jonkey@jonkey-mbp.local>
2016-04-19 21:52:41 +0800

    added foldLeft

---
+++ src/main/scala/FoldLeft.scala
@@ -0,0 +1,15 @@
+
+
+object FoldLeft {
+
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B =
+    as match {
+      case Nil    => z
+      case h :: t => foldLeft(t, f(z, h))(f)
+    }
+
+  def main(args: Array[String]): Unit = {
+    println(foldLeft(List(1, 2, 3, 4, 5), 0)(_ + _))
+    println(foldLeft(List(5), 0)(_ + _))
+  }
+}
