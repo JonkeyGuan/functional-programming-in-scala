d6b3641fd2ab42d63fedd491457a91054e2957c6
jonkey <jonkey@jonkey-mbp.local>
2016-04-22 20:24:32 +0800

    added concatenate

---
+++ src/main/scala/Concatenate.scala
@@ -0,0 +1,14 @@
+
+
+object Concatenate {
+
+  def concat[T](l: List[List[T]]): List[T] = FoldLeft.foldLeft(l, Nil: List[T])(AppendByFoldLeft.append)
+
+  def main(args: Array[String]): Unit = {
+
+    println(concat(List(List(1, 2, 3), List(4, 5, 6), List(7, 8, 9))))
+
+  }
+
+} 
+
