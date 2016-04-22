839540eb8ddd415c97b482f01bc658c1cdb962c0
jonkey <jonkey@jonkey-mbp.local>
2016-04-22 21:07:39 +0800

    added toString

---
+++ src/main/scala/ToString.scala
@@ -0,0 +1,13 @@
+
+
+object ToString {
+
+  def toString(l: List[Double]): List[String] = FoldRight.foldRight(l, Nil: List[String])((a: Double, b: List[String]) => a.toString() :: b)
+
+  def main(args: Array[String]): Unit = {
+
+    println(toString(List(1.010, 2.1, 3.2, 4.3, 5.4)))
+
+  }
+
+}
