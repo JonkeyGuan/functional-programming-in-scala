9cf614814e67dff5c2eb71823dd995673e2fe8ef
jonkey <jonkey@jonkey-mbp.local>
2016-04-23 10:03:43 +0800

    added tree and size

---
+++ src/main/scala/tree/Size.scala
@@ -0,0 +1,14 @@
+package tree
+
+object Size {
+
+  def size[A](t: Tree[A]): Int = t match {
+    case Leaf(_)      => 1
+    case Branch(l, r) => 1 + size(l) + size(r)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val tree = Branch(Leaf(1), Branch(Branch(Branch(Leaf(3), Branch(Leaf(5), Leaf(6))), Leaf(4)), Leaf(2)))
+    println(size(tree))
+  }
+}
