557c606511a9ba0c9b9cdaf83558d2a6ffc5eef3
jonkey <jonkey@jonkey-mbp.local>
2016-04-24 22:19:12 +0800

    added Stream and its toList

---
+++ src/main/scala/stream/Stream.scala
@@ -0,0 +1,46 @@
+package stream
+
+sealed trait Stream[+A] {
+
+  def toList0: List[A] = this match {
+    case Empty      => Nil
+    case Cons(h, t) => h() :: t().toList0
+  }
+
+  def toList: List[A] = {
+    @annotation.tailrec
+    def loop(s: Stream[A], acc: List[A]): List[A] = s match {
+      case Cons(h, t) => loop(t(), h() :: acc)
+      case _          => acc
+    }
+    loop(this, Nil).reverse
+  }
+
+}
+
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] = if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+
+}
+
+object Run {
+
+  def main(args: Array[String]): Unit = {
+    val s = Stream(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    println(s.toList0)
+    println(s.toList)
+  }
+
+}
