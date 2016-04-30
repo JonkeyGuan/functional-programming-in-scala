bf443c8edc6eec4c7e116dd97b0644532a1e3042
jonkey <jonkey@jonkey-mbp.local>
2016-04-30 20:52:10 +0800

    added intDouble, doubleInt and double3

--- src/main/scala/rng/RNG.scala
+++ src/main/scala/rng/RNG.scala
@@ -24,6 +24,24 @@ object RNG {
     val (i, r) = nonNegativeInt(rng)
     (i / (Int.MaxValue.toDouble + 1), r)
   }
+
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, r1) = rng.nextInt
+    val (d, r2) = double(rng)
+    ((i, d), r2)
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val ((i, d), r) = intDouble(rng)
+    ((d, i), r)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+    ((d1, d2, d3), r3)
+  }
 }
 
 object Run {
@@ -35,6 +53,9 @@ object Run {
 
     println(nonNegativeInt(rng))
     println(double(rng))
+    println(intDouble(rng))
+    println(doubleInt(rng))
+    println(double3(rng))
   }
 
 }
