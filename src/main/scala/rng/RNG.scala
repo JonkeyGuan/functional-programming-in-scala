09ea41cac32cfa376dd23b7e671641df52d314a2
jonkey <jonkey@jonkey-mbp.local>
2016-04-30 20:30:43 +0800

    added RNG and noNegativeInt

---
+++ src/main/scala/rng/RNG.scala
@@ -0,0 +1,35 @@
+package rng
+
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+object RNG {
+
+  case class Simple(seed: Long) extends RNG {
+    def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+      val nextRNG = Simple(newSeed)
+      val n = (newSeed >>> 16).toInt
+      (n, nextRNG)
+    }
+  }
+
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i, r) = rng.nextInt
+    (if (i < 0) -(i + 1) else i, r)
+  }
+
+}
+
+object Run {
+
+  def main(args: Array[String]): Unit = {
+    import RNG._
+    val rng = Simple(System.currentTimeMillis())
+    println(rng)
+
+    println(nonNegativeInt(rng))
+  }
+
+}
